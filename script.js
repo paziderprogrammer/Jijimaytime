dca5e1bad12d7828fa13a5e2670fd999ad5a3ea5
paziderprogrammer <158455569+paziderprogrammer@users.noreply.github.com>
2024-02-01 10:54:29 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function showMessage(response) {
 
     // Update text content and hide name message
     document.getElementById("question").textContent =
-      "The no button is just for visuals";
+      "Oh oh ich glaube du hast dich verklickt, aber nicht schlimm uwu";
     document.getElementById("name").style.display = "none";
 
     // Optional: You can also add a timeout to reset the position after a few seconds
@@ -34,7 +34,7 @@ function showMessage(response) {
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "LESGOOO see you on the 14th babygirl😘😘";
+    yesMessage.textContent = "LETSSSS GOOOOOOO, was willst du denn am 14. machen dann *frechgrins*?";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/dance.gif";
